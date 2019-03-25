b106663a36f255e5a1687caa00c18418c6adc3f9
Matthew White <matthew.white697@gmail.com>
2019-03-25 19:44:06 -0400

    fetch activity

---
+++ activities/beginner/using-fetch/README.md
@@ -0,0 +1,5 @@
+# Using Fetch
+
+Fetch is a modern browser implementation for fetching data -- think a new alternative to Ajax. Try fetching some data from an API and using that to alter your HTML.
+
+- [Docs](https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch)
---
+++ activities/beginner/using-fetch/src/example/script.js
@@ -0,0 +1,11 @@
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+    // return the json body from response
+    .then(response => response.json())
+    // do something with our data
+    .then(data => {
+        console.log('here', data);
+
+        // find an element on the page and replace inner html with response data
+        const p = document.getElementById('fetch-here');
+        p.innerHTML = data.title;
+    })
